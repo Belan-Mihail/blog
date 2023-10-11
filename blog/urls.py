5b073c167a43052fe05eb13fac6e0751c66c41ab
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-10-11 13:45:36 +0000

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 # 17 import and path
 urlpatterns = [
     path("", views.RecipesPostList.as_view(), name="home"),
+    # 35 add path to detail.html update index.html add links to the each posts, add check code post-detail
+    path('<slug:slug>/', views.RecipePostDetail.as_view(), name='recipe_post_detail'),
 ]
 # 18 urls.py project
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import RecipePost
 
+# 33 import View, get 404 function
+
 # 15 import RecipePost and generic
+# add comments
 class RecipesPostList(generic.ListView):
     model = RecipePost
     queryset = RecipePost.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
-    paginate_by = 6
+    paginate_by = 5
     context_object_name = 'recipes_post_list'
 
 # 16 index.html, base.html, css/style.css, js
 # 17 urls.py blog
+
+
+# 34 add class RecipesPostDetail and add post_details
+# add comment
+# 35 urls
+class RecipePostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = RecipePost.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+            },
+        )
